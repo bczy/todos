ef3c37a62802e5ee910f8c6dc9a7ecfa81a6084e
trambz <bertrand.coizy@gmail.com>
2020-10-20 09:54:47 +0200

    Updating menu links

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,4 +1,4 @@
-import { Navbar, Nav, Image, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, Image } from "react-bootstrap";
 
 const Navigation = () => 
 <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
@@ -12,25 +12,11 @@ const Navigation = () =>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link href="#features">About</Nav.Link>
+          <Nav.Link href="#pricing">Settings</Nav.Link>
         </Nav>
         <Nav>
-          <Nav.Link href="#deets">More deets</Nav.Link>
-          <Nav.Link eventKey={2} href="#memes">
-            Dank memes
-          </Nav.Link>
+          <Nav.Link target="blank" href="https://github.com/bczy/todos">GitHub</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
