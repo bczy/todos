d2983e7e00c96d25631ed9f0172750852c1ac5b6
trambz <bertrand.coizy@gmail.com>
2020-10-26 10:35:46 +0100

    Adding form validation

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -1,15 +1,28 @@
 import React, { useState } from 'react'
 import { Button, Card, Col, Container, Form } from 'react-bootstrap'
 import { connect, useDispatch } from 'react-redux';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
 import { fetchAddTodo } from '../store/actions/todos';
+
+const TodoSchema = Yup.object().shape({
+    title: Yup.string()
+      .min(2, 'Too Short! Should be between 2 and 32 characters.')
+      .max(32, 'Too Long! Should be between 2 and 32 characters.')
+      .required('Required'),
+ 
+    description: Yup.string()
+      .min(5, 'Too Short! Should be between 5 and 128 characters.')
+      .max(128, 'Too Long! Should be between 5 and 128 characters.')
+      .required('Required'),
+ 
+  });
 
 const AddTodo = ({items, isSubmiting}) => {
     const dispatch = useDispatch();
-    const [ title, setTitle ] = useState('');
-    const [ description, setDescription ] = useState('');
-    
-    function handleAddTodo(e){
-        e.preventDefault();
+
+    function handleAddTodo({title, description}){
         dispatch(fetchAddTodo({title, description}, items));
     }
 
@@ -17,30 +30,62 @@ const AddTodo = ({items, isSubmiting}) => {
         <Card border="light">
           <Card.Body>
             <Card.Title>Add todo</Card.Title>
-            <Form method="POST">
-                <Form.Row>
-                    <Form.Group as={Col} controlId="todo-title">
-                        <Form.Label>Title</Form.Label>
-                        <Form.Control 
-                            required
-                            onChange={e => setTitle(e.target.value)} 
-                            placeholder="Enter title" />
-                    </Form.Group>
-
-                    <Form.Group as={Col} controlId="formGridPassword">
-                        <Form.Label>Description</Form.Label>
-                        <Form.Control 
-                            onChange={e => setDescription(e.target.value)} 
-                            placeholder="Enter description" />
-                    </Form.Group>
-
-                    <Form.Group as={Col} controlId="formGridPassword">
-                        <Button enabled={isSubmiting} variant="primary" type="submit" onClick={handleAddTodo}>
-                            Submit
-                        </Button>
-                    </Form.Group>
-                    </Form.Row>
-                </Form>
+                <Formik
+                    initialValues={{
+                        title: '',
+                        description: '',
+                    }}
+                    validationSchema={TodoSchema}
+                    onSubmit={values => {
+                        handleAddTodo(values)
+                    }}
+                    >
+                    {({ handleSubmit,
+                        handleChange,
+                        values,
+                        touched,
+                        errors }) => (
+                        <Form method="POST" noValidate onSubmit={handleSubmit}>
+                            <Form.Row>
+                                <Form.Group as={Col} controlId="validationTitle">
+                                    <Form.Label>Title</Form.Label>
+                                    <Form.Control 
+                                        name="title"
+                                        required
+                                        onChange={handleChange}
+                                        value={values.title}
+                                        isValid={touched.title && !errors.title}
+                                        isInvalid={errors.title}
+                                        placeholder="Enter title" />
+                                    <Form.Control.Feedback tooltip type="invalid">
+                                        Please choose a todo title.
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="validationDescription">
+                                    <Form.Label>Description</Form.Label>
+                                    <Form.Control 
+                                        name="description"
+                                        required
+                                        onChange={handleChange}
+                                        value={values.description}
+                                        isValid={touched.description && !errors.description}
+                                        isInvalid={errors.description}
+                                        placeholder="Enter description" />
+                                    <Form.Control.Feedback tooltip type="invalid">
+                                        Please choose a todo description.
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                            </Form.Row>
+                            <Form.Row>
+                                <Form.Group as={Col} controlId="formGridPassword">
+                                    <Button variant="primary" type="submit" >
+                                        Submit
+                                    </Button>
+                                </Form.Group>
+                            </Form.Row>
+                        </Form>
+                    )}
+                </Formik>
             </Card.Body>
         </Card>
     </Container>
