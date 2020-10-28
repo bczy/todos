b54711e161b6a0b0c13d0a61b953e42266f587e2
trambz <bertrand.coizy@gmail.com>
2020-10-28 15:57:12 +0100

    Update build config

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingSlash: true,
+  exportPathMap: async function () {
+    const paths = {
+      '/': { page: '/' },
+      '/api': { page: '/api' },
+    };
+
+    return paths;
+  },
+};
