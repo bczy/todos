fabb673b463c66c1fde2789bbcc2beeac51074f5
trambz <bertrand.coizy@gmail.com>
2020-11-05 20:33:19 +0100

    adding custom build for azure

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  trailingSlash: true,
-  exportPathMap: async function () {
-    const paths = {
-      '/': { page: '/' },
-    };
+	trailingSlash: true,
+	exportPathMap: async function () {
+		const paths = {
+			'/': { page: '/' },
+			'/api': { page: '/api' },
+		};
 
-    return paths;
-  },
+		return paths;
+	},
 };
